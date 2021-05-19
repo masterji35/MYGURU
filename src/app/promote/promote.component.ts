3ba1e1de0030fde9f28069d9b7ca7be0fa1df61a
somwanshi.dnyaneshwar@gmail.com <Mahatma@352>
2021-05-19 11:23:48 +0530

    Initial commit

---
+++ src/app/promote/promote.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngx-promote',
+  templateUrl: './promote.component.html',
+  styleUrls: ['./promote.component.scss']
+})
+export class PromoteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
